bb37dcb4b11ee1fbdcd972fe6cbfaccdb51966c0
cnmicha <evsc@gmx.de>
2015-03-12 22:52:46 +0100

    insertion sort
    insertion sort beim einlesen nach Ort

--- ukfzliste.pas
+++ ukfzliste.pas
@@ -3,249 +3,276 @@ unit uKfzListe;
 {$mode objfpc}{$H+}
 
 interface
+
 uses
   Classes, SysUtils, strutils, uDatensatz, Dialogs;
 
-const maxAnz= 600; //array length
+const
+  maxAnz = 600; //array length
 
-TYPE TListe = class
-                private
-                       liste: ARRAY [0..maxAnz-1] OF TDatensatz;
-                       anzahl: CARDINAL;
-                public
-                      constructor create;
-                      destructor destroy;
-                      procedure einlesen (datname: String);
-                      procedure einlesenFilterKennzeichen (datname,query: String);
-                      procedure einlesenFilterOrt (datname,query: String);
-                      function binsuche (query: String):INTEGER;
-                      procedure einlesenFilterBundesland (datname,query: String);
-                      function getAnz():CARDINAL;
-                      function getKennzeichen(Itemindex:CARDINAL):STRING;
-                      function getOrt(ItemIndex:CARDINAL):STRING;
-                      function getBundesland(ItemIndex:CARDINAL):STRING;
-     end;
+type
+  TListe = class
+  private
+    liste: array [0..maxAnz - 1] of TDatensatz;
+    anzahl: cardinal;
+  public
+    constructor Create;
+    destructor Destroy;
+    procedure einlesen(datname: string);
+    procedure einlesenFilterKennzeichen(datname, query: string);
+    procedure einlesenFilterOrt(datname, query: string);
+    function binsuche(query: string): integer;
+    procedure einlesenFilterBundesland(datname, query: string);
+    function getAnz(): cardinal;
+    function getKennzeichen(ItemIndex: cardinal): string;
+    function getOrt(ItemIndex: cardinal): string;
+    function getBundesland(ItemIndex: cardinal): string;
+  end;
 
 
 
 implementation
 
-constructor TListe.create;
+constructor TListe.Create;
 begin
 
 end;
 
-destructor TListe.destroy;
+destructor TListe.Destroy;
 begin
 
 end;
 
-procedure TListe.einlesen(datname:string);
-var f : TextFile;
-aktuell: CARDINAL;
-str : string;
-data:TStringList;
+procedure TListe.einlesen(datname: string);
+var
+  f: TextFile;
+  aktuell, j: cardinal;
+  str: string;
+  Data: TStringList;
 begin
-  AssignFile(f,datname);
+  AssignFile(f, datname);
   reset(f); //lock file
-  aktuell:=0;
+  aktuell := 0;
 
-  while not EOF (f) do
+  while not EOF(f) do
   begin
-    readln(f,str);
+    readln(f, str);
 
-    data:= NIL; //truncate array
-    data:= TStringList.Create;
-    data.Delimiter := ';';
-    data.StrictDelimiter := True; //space is not a delimiter
-    data.DelimitedText := str;
+    //split str into array seperated by ";"
+    Data := nil; //truncate array
+    Data := TStringList.Create;
+    Data.Delimiter := ';';
+    Data.StrictDelimiter := True; //space is not a delimiter
+    Data.DelimitedText := str;
+
 
     //BEGIN insertion sort
 
-    // ...
-
-    liste[aktuell]:= TDatensatz.create;
-    liste[aktuell].definieren(data[0],data[1],data[2]);
+    j := aktuell;
+    if length(liste) > 0 then
+    begin
+      while (j > 1) and (liste[j - 1].ort > Data[1]) do
+      begin
+        liste[j] := liste[j - 1];
+        Dec(j);
+      end;
+      liste[j] := TDatensatz.Create;
+      liste[j].definieren(Data[0], Data[1], Data[2]);
+    end
+    else
+    begin
+      liste[0] := TDatensatz.Create;
+      liste[0].definieren(Data[0], Data[1], Data[2]);
+    end;
 
     //END insertion sort
 
-    INC(aktuell);
+
+    Inc(aktuell);
   end;
 
   CloseFile(f);
-  anzahl:= aktuell;
+  anzahl := aktuell;
 end;
 
-procedure TListe.einlesenFilterKennzeichen (datname,query: String);
-var f : TextFile;
-aktuell: CARDINAL;
-str : string;
-data:TStringList;
+procedure TListe.einlesenFilterKennzeichen(datname, query: string);
+var
+  f: TextFile;
+  aktuell: cardinal;
+  str: string;
+  Data: TStringList;
 begin
-  AssignFile(f,datname);
+  AssignFile(f, datname);
   reset(f);
-  aktuell:=0;
+  aktuell := 0;
 
-  while not EOF (f) do
+  while not EOF(f) do
   begin
-    readln(f,str);
+    readln(f, str);
 
-    data:= NIL; //truncate array
-    data:= TStringList.Create;
-    data.Delimiter := ';';
-    data.StrictDelimiter := True; //space is not a delimiter
-    data.DelimitedText := str;
+    Data := nil; //truncate array
+    Data := TStringList.Create;
+    Data.Delimiter := ';';
+    Data.StrictDelimiter := True; //space is not a delimiter
+    Data.DelimitedText := str;
 
-    if(AnsiContainsStr(LowerCase(data[0]), LowerCase(query))) then
+    if (AnsiContainsStr(LowerCase(Data[0]), LowerCase(query))) then
     begin
-      liste[aktuell]:= TDatensatz.create;
-      liste[aktuell].definieren(data[0],data[1],data[2]);
+      liste[aktuell] := TDatensatz.Create;
+      liste[aktuell].definieren(Data[0], Data[1], Data[2]);
 
-      INC(aktuell);
+      Inc(aktuell);
     end;
   end;
 
   CloseFile(f);
-  anzahl:= aktuell;
-  aktuell:= 0;
+  anzahl := aktuell;
+  aktuell := 0;
 
 end;
 
-procedure TListe.einlesenFilterOrt (datname,query: String);
-var f : TextFile;
-aktuell: CARDINAL;
-str : string;
-var data:TStringList;
+procedure TListe.einlesenFilterOrt(datname, query: string);
+var
+  f: TextFile;
+  aktuell: cardinal;
+  str: string;
+var
+  Data: TStringList;
 begin
-  AssignFile(f,datname);
+  AssignFile(f, datname);
   reset(f);
-  aktuell:=0;
+  aktuell := 0;
 
-  while not EOF (f) do
+  while not EOF(f) do
   begin
-    readln(f,str);
+    readln(f, str);
 
-    data:= NIL; //truncate array
-    data:= TStringList.Create;
-    data.Delimiter := ';';
-    data.StrictDelimiter := True; //space is not a delimiter
-    data.DelimitedText := str;
+    Data := nil; //truncate array
+    Data := TStringList.Create;
+    Data.Delimiter := ';';
+    Data.StrictDelimiter := True; //space is not a delimiter
+    Data.DelimitedText := str;
 
-    if(AnsiContainsStr(LowerCase(data[1]), LowerCase(query))) then
+    if (AnsiContainsStr(LowerCase(Data[1]), LowerCase(query))) then
     begin
-      liste[aktuell]:= TDatensatz.create;
-      liste[aktuell].definieren(data[0],data[1],data[2]);
+      liste[aktuell] := TDatensatz.Create;
+      liste[aktuell].definieren(Data[0], Data[1], Data[2]);
 
-      INC(aktuell);
+      Inc(aktuell);
     end;
   end;
 
   CloseFile(f);
-  anzahl:= aktuell;
-  aktuell:= 0;
+  anzahl := aktuell;
+  aktuell := 0;
 
 end;
 
-function TListe.binsuche (query: String):INTEGER;
-function mid(u,d:cardinal):CARDINAL;
-begin
-  result:=(u+d) div 2;
-end;
+function TListe.binsuche(query: string): integer;
 
-var f : TextFile;
-aktuell: CARDINAL;
-str : string;
-low, up, splitCount : cardinal;
+  function mid(u, d: cardinal): cardinal;
+  begin
+    Result := (u + d) div 2;
+  end;
+
+var
+  f: TextFile;
+  aktuell: cardinal;
+  str: string;
+  low, up, splitCount: cardinal;
 
 begin
 
   low := 0;
-  up := aktuell-1; //max anzahl
-  splitCount:=1;
+  up := aktuell - 1; //max anzahl
+  splitCount := 1;
 
-  while not (LowerCase(liste[mid(up,low)].ort) = LowerCase(query)) do
+  while not (LowerCase(liste[mid(up, low)].ort) = LowerCase(query)) do
   begin
-    if up-low = 0 then
+    if up - low = 0 then
     begin
       ShowMessage('str not found');
       exit;
     end;
 
 
-    if LowerCase(liste[mid(up,low)].ort) > LowerCase(query) then
+    if LowerCase(liste[mid(up, low)].ort) > LowerCase(query) then
     begin
-      up:=mid(up,low);
-      if up-low = 1 then dec(up);
+      up := mid(up, low);
+      if up - low = 1 then
+        Dec(up);
     end
     else
     begin
-      low:=mid(up,low);
-      if up-low = 1 then inc(up);
+      low := mid(up, low);
+      if up - low = 1 then
+        Inc(up);
     end;
 
-    inc(splitCount);
+    Inc(splitCount);
   end;
 
   ShowMessage('found string "' + query + '" using ' + IntToStr(splitCount) + ' splits');
 
-  anzahl:= 1;
-  aktuell:= 0;
+  anzahl := 1;
+  aktuell := 0;
 end;
 
-procedure TListe.einlesenFilterBundesland (datname,query: String);
-var f : TextFile;
-aktuell: CARDINAL;
-str : string;
-var data:TStringList;
+procedure TListe.einlesenFilterBundesland(datname, query: string);
+var
+  f: TextFile;
+  aktuell: cardinal;
+  str: string;
+var
+  Data: TStringList;
 begin
-  AssignFile(f,datname);
+  AssignFile(f, datname);
   reset(f);
-  aktuell:=0;
+  aktuell := 0;
 
-  while not EOF (f) do
+  while not EOF(f) do
   begin
-    readln(f,str);
+    readln(f, str);
 
-    data:= NIL; //truncate array
-    data:= TStringList.Create;
-    data.Delimiter := ';';
-    data.StrictDelimiter := True; //space is not a delimiter
-    data.DelimitedText := str;
+    Data := nil; //truncate array
+    Data := TStringList.Create;
+    Data.Delimiter := ';';
+    Data.StrictDelimiter := True; //space is not a delimiter
+    Data.DelimitedText := str;
 
-    if(AnsiContainsStr(LowerCase(data[2]), LowerCase(query))) then
+    if (AnsiContainsStr(LowerCase(Data[2]), LowerCase(query))) then
     begin
-      liste[aktuell]:= TDatensatz.create;
-      liste[aktuell].definieren(data[0],data[1],data[2]);
+      liste[aktuell] := TDatensatz.Create;
+      liste[aktuell].definieren(Data[0], Data[1], Data[2]);
 
-      INC(aktuell);
+      Inc(aktuell);
     end;
   end;
 
   CloseFile(f);
-  anzahl:= aktuell;
-  aktuell:= 0;
+  anzahl := aktuell;
+  aktuell := 0;
 
 end;
 
-function TListe.getAnz():CARDINAL;
+function TListe.getAnz(): cardinal;
 begin
-  result:= self.anzahl;
+  Result := self.anzahl;
 end;
 
-function TListe.getKennzeichen(Itemindex:CARDINAL):STRING;
+function TListe.getKennzeichen(ItemIndex: cardinal): string;
 begin
-  result:= liste[ItemIndex].kennzeichen;
+  Result := liste[ItemIndex].kennzeichen;
 end;
 
-function TListe.getOrt(Itemindex:CARDINAL):STRING;
+function TListe.getOrt(ItemIndex: cardinal): string;
 begin
-  result:= liste[ItemIndex].ort;
+  Result := liste[ItemIndex].ort;
 end;
 
-function TListe.getBundesland(Itemindex:CARDINAL):STRING;
+function TListe.getBundesland(ItemIndex: cardinal): string;
 begin
-  result:= liste[ItemIndex].bundesland;
+  Result := liste[ItemIndex].bundesland;
 end;
 
 end.
-
